04bc32b0241372aeb59ec5b032c3eab8e1931d5b
Miloslav Trmač <mitr@redhat.com>
2026-08-06 21:10:40 +0200

    Remove unnecessary filepath.FromSlash
    Everything else uses the path as is (and it already comes
    from filepath.Join, which, via filepath.Clean, implicitly
    does FromSlash).
    
    Should not, ultimately, change behavior.
    
    Signed-off-by: Miloslav Trmač <mitr@redhat.com>

--- copier/copier.go
+++ copier/copier.go
@@ -2240,14 +2240,14 @@ func copierHandlerPut(bulkReader io.Reader, req request, idMappings *idtools.IDM
 				//	todo: the general solution requires resolving to an absolute path, handling
 				//	renaming, and then possibly converting back to a relative symlink
 				// }
-				if err = os.Symlink(filepath.FromSlash(hdr.Linkname), filepath.FromSlash(path)); err != nil && errors.Is(err, os.ErrExist) {
+				if err = os.Symlink(filepath.FromSlash(hdr.Linkname), path); err != nil && errors.Is(err, os.ErrExist) {
 					if req.PutOptions.NoOverwriteDirNonDir {
 						if st, err := os.Lstat(path); err == nil && st.IsDir() {
 							break
 						}
 					}
 					if err = os.RemoveAll(path); err == nil {
-						err = os.Symlink(filepath.FromSlash(hdr.Linkname), filepath.FromSlash(path))
+						err = os.Symlink(filepath.FromSlash(hdr.Linkname), path)
 					}
 				}
 			case tar.TypeChar:
